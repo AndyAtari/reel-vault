58db4a50a323c7339f885bc6df156b0f9585275a
Andrew Alteri <andrewsalteri@gmail.com>
2020-07-12 15:31:01 -0500

    If you merge this commit, it will create a UserController and FilmsController.

---
+++ app/controllers/FilmsController.rb
@@ -0,0 +1,16 @@
+class FilmController < ApplicationController
+    #index action
+
+    #new action
+
+    #create action
+
+    #show action
+
+    #edit action
+
+    #update action
+
+    #delete action
+    
+end
