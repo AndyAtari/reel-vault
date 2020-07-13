a914f19158db11824fabddf188894e54cd294965
Andrew Alteri <andrewsalteri@gmail.com>
2020-07-13 11:36:57 -0500

    If you merge this commit, it will add a create and show action.
    This commit posts a simple WIP form and redirects to the films of the current user using a find_by_id(params).

--- app/controllers/FilmsController.rb
+++ app/controllers/FilmsController.rb
@@ -4,11 +4,24 @@ class FilmsController < ApplicationController
         @films = current_user.films 
         erb :'films/index'
     end
+
     #new action(view for form that will create)
+    get '/films/new' do 
+        erb :'films/new'
+    end
 
     #create action
+    post '/films' do
+        film = current_user.films.build(params) 
+        film.save 
+        redirect "films/#{film.id}"
+    end
 
     #show action
+    get '/films/:id' do
+        @film = current_user.films.find_by_id(params[:id])
+        erb :'films/show'
+    end
 
     #edit action(view for form that will update)
 
