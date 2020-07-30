d872ceff5f2f2b1a1f825a4b257393d7eb5040d5
Andrew Alteri <andrewsalteri@gmail.com>
2020-07-30 12:26:46 -0500

    If you merge this commit, a new method will validate user login format.
    This commit will add a method that validates user_name and email format. The User model will validate that a new user does not have whitespace in User name or email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password 
     has_many :films 
-    validates :user_name, :email, uniqueness: true, presence: true 
+    validates :user_name, :email, uniqueness: true, presence: true, format: { without: /\s/ }
 
 end
